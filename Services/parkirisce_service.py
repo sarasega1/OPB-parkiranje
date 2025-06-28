ade589ffaa59c0bb7c735742dd23f445c9df8ef5
Sara Šega <ss46025@student.uni-lj.si>
2025-06-28 11:22:50 +0200

    urejanje

--- Services/parkirisce_service.py
+++ Services/parkirisce_service.py
@@ -4,7 +4,6 @@ from typing import List
 from datetime import datetime, timedelta
 
 
-
 class ParkirisceService:
     def __init__(self) -> None:
         self.repo = Repo()
@@ -15,7 +14,6 @@ class ParkirisceService:
     def dobi_oseboDto(self) -> List[Oseba]:
         return self.repo.dobi_oseboDto()
 
-    
     def dobi_parkirisca(self) -> List[Parkirisce]:               
         return self.repo.dobi_parkirisca()
     
@@ -25,9 +23,6 @@ class ParkirisceService:
     def naredi_rezervacijo(self, lokacija: str,  mesto_id: int, uporabnisko_ime: str, registrska_stevilka: str, prihod_str: str, odhod_str: str) -> None:
         prihod = datetime.strptime(prihod_str, "%Y-%m-%dT%H:%M")
         odhod = datetime.strptime(odhod_str, "%Y-%m-%dT%H:%M")
-        sedaj = datetime.now()
-
-
 
         if odhod <= prihod:
             raise ValueError("Odhod mora biti kasneje od prihoda!")
@@ -46,10 +41,8 @@ class ParkirisceService:
         return self.repo.dobi_rezervacije()
 
     def obstaja_oseba(self, uporabnisko_ime: str) -> bool:
-    
         return self.repo.obstaja_oseba(uporabnisko_ime)
 
-        
     def dobi_zasedena_mesta(self,lokacija: str) -> List[int]:
             return self.repo.dobi_zasedena_mesta(lokacija)
 
@@ -62,21 +55,15 @@ class ParkirisceService:
     def dobi_rezervacije_uporabnika(self, uporabnisko_ime: str) -> List[Rezervacija]:
         return self.repo.dobi_rezervacije_uporabnika(uporabnisko_ime)
 
-
-
-
     def podaljsaj_rezervacijo_po_kljucih(self, lokacija: str, id_parkirnega_mesta: int, prihod: datetime, minute: int):
         rezervacija = self.repo.dobi_rezervacijo_po_kljucih(lokacija, id_parkirnega_mesta, prihod)
-       
         now = datetime.now()
-
-    # če je trenutni odhod v prihodnosti, podaljšaj od njega naprej
+        # če je trenutni odhod v prihodnosti, podaljšaj od njega naprej
         if rezervacija.odhod > now:
             rezervacija.odhod += timedelta(minutes=minute)
         else:
         # če je že potekla, podaljšaj od zdaj naprej
             rezervacija.odhod = now + timedelta(minutes=minute)
-
         self.repo.posodobi_rezervacijo_po_kljucih(rezervacija)
 
     def odstrani_rezervacijo_po_kljucih(self, lokacija: str, id_parkirnega_mesta: int, prihod: datetime) -> None:
